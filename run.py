e73f8113f649a7db737f634a34c97a1305b98bc3
OksanaPetriv <oksana.petriv17@gmail.com>
2021-09-06 19:52:16 +0300

    edit bg

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-# -*- coding: UTF-8 -*-
-"""
-hello_flask: First Python-Flask webapp
-"""
 from app import app
 
+
 if __name__ == '__main__':
-  # Script executed directly?
-    app.run(debug=True)  # Launch built-in web server and run this Flask webapp
+	app.run(debug=True)
